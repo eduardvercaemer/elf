280ceadc84f58fb82d7d0a7b75bf6650b186621e
Eduard Vercaemer <vercaemereduard@gmail.com>
2020-12-10 23:54:57 -0600

    added needed unwraps

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,25 @@ pub mod util {
     /// Read one byte.
     pub fn read_u8(file: &mut File) -> u8 {
         let mut buf = [0u8; 1];
-        file.read(&mut buf);
+        file.read(&mut buf).unwrap();
         u8::from_ne_bytes(buf)
     }
     /// Read two bytes.
     pub fn read_u16(file: &mut File) -> u16 {
         let mut buf = [0u8; 2];
-        file.read(&mut buf);
+        file.read(&mut buf).unwrap();
         u16::from_ne_bytes(buf)
     }
     /// Read four bytes.
     pub fn read_u32(file: &mut File) -> u32 {
         let mut buf = [0u8; 4];
-        file.read(&mut buf);
+        file.read(&mut buf).unwrap();
         u32::from_ne_bytes(buf)
     }
     /// Read eight bytes.
     pub fn read_u64(file: &mut File) -> u64 {
         let mut buf = [0u8; 8];
-        file.read(&mut buf);
+        file.read(&mut buf).unwrap();
         u64::from_ne_bytes(buf)
     }
 }
